419aee190b222c85a096cdce96096cda4c893423
dhruv <nidhi.singh.aws@gmail.com>
2026-04-08 13:30:02 +0530

    changes in assignment 3

--- assignment-4-april/AprilFirstWeekAssignment3.java
+++ assignment-4-april/AprilFirstWeekAssignment3.java
@@ -5,28 +5,48 @@ public class AprilFirstWeekAssignment3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Q3. Move All Zeros to End
-		boolean x=false;
-		
-		//test for below values
-		//int[] arr = {4,7,0,5,23,0,20};		
-		//int[] arr = {0,7,0,5,23,0,20};
-		int[] arr = {0,0,89,5,23,0,20};
-		
-		for(int i=0;i<arr.length;i++) {
-			for(int j=i+1;j<arr.length;j++) {
-				if(arr[i]==0) {
-					int temp = arr[i];
-					arr[i]= arr[j];
-					arr[j]=temp;
-				
-				}				
-			}
-		
+		int[] arr1= {0,0,0,0,77,0,1,0,0,0};
+        int length1 = arr1.length;
+	 
+        int zero  = findOutZeros(arr1);
+		 // System.out.println(zero);
+		  int counter1=0;
+		  for(int i=0;i<length1;) {
+		  	   
+		  	  // System.out.println(i);
+		  	   for(int j=i;j<arr1.length-1; j++) {
+		     		  if(arr1[j]==0) {
+		     			  int temp = arr1[j];
+		     			  arr1[j]=arr1[j+1];
+		     			  arr1[j+1]=temp;
+		     		  }
+		         }
+		  	
+		  	   if(arr1[i]!=0) {
+		  		   i++;
+		  	   }
+		  	   
+		  	   
+		  	   
+		  	   //this is very important condition
+	            if(i>=(length1-zero)) {
+		  		   break;
+		  	   }
+		  	   
+		  
 		}
-		for(int i=0;i<arr.length;i++) {
-			System.out.println(arr[i]);
-			
+	   	for(int i=0;i<arr1.length; i++) {
+	        	System.out.println(arr1[i]);
+	    }
+	        	
+	}
+	static int findOutZeros(int arr[]){
+		int count=0;
+		for(int j=0;j<arr.length;j++) {
+			if(arr[j]==0)
+				count++;
 		}
+		return count;
 	}
 
 }
