026802031d129e8b6591f90629d587640f5fdc7a
dhruv <nidhi.singh.aws@gmail.com>
2026-04-08 12:14:03 +0530

    changes in assignment

--- assignment-4-april/AprilFirstWeekAssignment1.java
+++ assignment-4-april/AprilFirstWeekAssignment1.java
@@ -4,27 +4,33 @@ public class AprilFirstWeekAssignment1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			int[] arr = {4, 2, 7, 2, 9, 1};
+			int[] arr = {4, 4, 7, 2, 9, 45};
 			//new int[10];
 			
-			//find the smallest element in array
-			for(int i=0;i<arr.length;i++) {
-				for(int j=i+1;j<arr.length;j++) {
-					if(arr[i]>arr[j])
-					{
-						int temp=arr[i];
-						arr[i]=arr[j];
-						arr[j]=temp;
-					}
+			
+			int smallest=arr[0];
+			for(int j=1;j<arr.length;j++) {
+				if(smallest>arr[j]) {
+					smallest=arr[j];
 				}
+					
 			}
-			
-			for(int i=0;i<arr.length;i++) {
+			//second smallest
+			int secondSmallest=arr[0];
+			for(int j=1;j<arr.length;j++) {
+				if(arr[j] != smallest)
+					if(secondSmallest > arr[j]) {
+						
+						
+					secondSmallest=arr[j];
+				}
 				
-			   System.out.println(arr[i]);
+					
 			}
 			
-			System.out.println("Second smallest is >>" + arr[1]);
+			
+			System.out.println("SMALLEST" + smallest);
+			System.out.println("SECONDSMALLEST" + secondSmallest);
 	}
 
 }
